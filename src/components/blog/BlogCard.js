0cb2c471a820a52c79f48d8bcc9aefab70f59259
Ypper <andreas.ypper@gmail.com>
2022-06-29 10:33:35 +0200

    Added styling for mobile for blog cards.

--- src/components/blog/BlogCard.js
+++ src/components/blog/BlogCard.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
 
-export default function BlogCard({blog}) {
+export default function BlogCard({ blog }) {
+  const [show, setShow] = React.useState(false);
+  const [blogBody, setBlogBody] = React.useState(
+    blog.body.substring(0, 100) + "..."
+  );
+  React.useEffect(() => {
+    if (!show) {
+      if (blog.body.length > 100) {
+        setBlogBody(blog.body.substring(0, 100) + "...");
+      }
+    } else {
+      setBlogBody(blog.body);
+    }
+  }, [blog.body, show]);
   return (
-    <div className='blog-card-container'>
-        <h1>{blog.title}</h1>
-        <p>{blog.body}</p>
+    <div className="blog-card-container" onClick={() => setShow(!show)}>
+      <h1>{blog.title}</h1>
+      <p>{blogBody}</p>
     </div>
-  )
+  );
 }
